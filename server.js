5c290329afa15319e98f1e19e10da699238d7b29
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-13 15:32:17 -0400

    Imported router

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
+const router = require("./controllers/burgers_controller");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,3 +14,5 @@ app.set("view engine", "handlebars");
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
+
+app.use(router);
