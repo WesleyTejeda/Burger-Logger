405d8f6c6d57738432d955d67a3f07965cb27e13
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-13 19:32:28 -0400

    Defined query structures for ORM

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,38 @@
 const connection = require("./connect.js");
 
+let questionMarkNum = (num) => {
+    let numArr = [];
+    for(let i=0; i < num; i++) {
+        numArr.push("?");
+    }
+    // return 
+}
+
+const orm = {
+    selectAll: (table, cb) =>{
+        let query = `SELECT * FROM ${table};`;
+        connection.query(query, (err, result) => {
+            if (err)
+                throw err;
+            cb(result);
+        })
+    },
+    insertOne: (table, cols, vals, cb) =>{
+        let query = `INSERT INTO ${table}(${cols}) VALUES(${vals});`;
+        connection.query(query, (err, result) => {
+            if (err)
+                throw err;
+            cb(result);
+        })
+    },
+    updateOne: (table, objColVals, condition, cb) =>{
+        let query = `UPDATE ${table} SET ${objColVals} WHERE ${condition};`
+        connection.query(query,(err, result) => {
+            if (err)
+                throw err;
+            cb(result);
+        })
+    }
+}
+
+module.exports = orm;
