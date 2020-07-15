86c0e76fb0ec16b66b237596cab28f10236723b4
Wesley PC <wesley.tejeda95@gmail.com>
2020-07-14 21:48:50 -0400

    Updated typo

--- config/connect.js
+++ config/connect.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 require("dotenv").config();
 var connection;
 if(process.env.JAWSDB_URL){
-  connection = mysql.createConnect(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
